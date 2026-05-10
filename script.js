a9b4cecf40bacc268ff9503f79d9ec6697d01681
djabranemmd <mammadi.a.djabrane@gmail.com>
2026-05-10 21:48:23 +0100

    write JS code and solve some error/bugs after this I  Connect app with REST Countries API

---
+++ script.js
@@ -0,0 +1,99 @@
+// Select Elements
+const countryInput = document.getElementById("countryInput");
+
+const searchBtn = document.getElementById("searchBtn");
+
+const randomBtn = document.getElementById("randomBtn");
+
+const countryFlag = document.getElementById("countryFlag");
+
+const countryName = document.getElementById("countryName");
+
+const capital = document.getElementById("capital");
+
+const population = document.getElementById("population");
+
+const currency = document.getElementById("currency");
+
+const language = document.getElementById("language");
+
+const region = document.getElementById("region");
+
+// Function: Fetch Country Data
+async function getCountryData(country) {
+  try {
+    // API Request
+    const response = await fetch(
+      `https://restcountries.com/v3.1/name/${country}`,
+    );
+
+    // Convert Response To JSON
+    const data = await response.json();
+
+    // First Country Result
+    const countryData = data[0];
+
+    // Country Flag
+    countryFlag.src = countryData.flags.svg;
+
+    // Country Name
+    countryName.textContent = countryData.name.common;
+
+    // Capital
+    capital.textContent = countryData.capital
+      ? countryData.capital[0]
+      : "No Capital";
+
+    // Population
+    population.textContent = countryData.population.toLocaleString();
+
+    // Currency
+    currency.textContent = Object.values(countryData.currencies)[0].name;
+
+    // Language
+    language.textContent = Object.values(countryData.languages).join(", ");
+
+    // Region
+    region.textContent = countryData.region;
+  } catch (error) {
+    alert("Country not found!");
+
+    console.log(error);
+  }
+}
+
+// Search Button Click
+searchBtn.addEventListener("click", () => {
+  const country = countryInput.value.trim();
+
+  if (country !== "") {
+    getCountryData(country);
+  }
+});
+
+// Enter Key Support
+countryInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    searchBtn.click();
+  }
+});
+
+// Random Country Button
+randomBtn.addEventListener("click", () => {
+  const countries = [
+    "Algeria",
+    "France",
+    "Brazil",
+    "Japan",
+    "Canada",
+    "Italy",
+    "Germany",
+    "India",
+    "Turkey",
+    "Australia",
+  ];
+
+  const randomCountry = countries[Math.floor(Math.random() * countries.length)];
+
+  getCountryData(randomCountry);
+});
